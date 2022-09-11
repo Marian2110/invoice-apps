542ee082265065af764e8175f6eec15bc8aa0c20
Marian Datcu <Marian.Datcu@partner.bmwgroup.com>
2022-09-11 18:21:43 +0300

    asd

--- payment-application/payment-service/src/main/java/ro/fastrackit/paymentservice/model/mapper/PaymentMapper.java
+++ payment-application/payment-service/src/main/java/ro/fastrackit/paymentservice/model/mapper/PaymentMapper.java
@@ -1,13 +1,15 @@
 package ro.fastrackit.paymentservice.model.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 import ro.fastrackit.paymentservice.model.entity.PaymentEntity;
 import ro.fasttrackit.payments.client.dto.Payment;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface PaymentMapper {
+    PaymentMapper INSTANCE = Mappers.getMapper(PaymentMapper.class);
     List<Payment> toApi(List<PaymentEntity> students);
 
     Payment toApi(PaymentEntity student);
